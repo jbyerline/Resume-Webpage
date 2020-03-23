67dc0a9f3d3e97abdb2a352a5b31fda2c2b9530c
Jacob Byerline <jbyerline@gmail.com>
2020-03-22 19:57:26 -0700

    Updates Made Everywhere

--- Webpage/html/javascript/index.js
+++
@@ -1,3 +0,0 @@
-document.getElementById('date').innerHTML = new Date().toDateString();
-var x = 10;
-document.getElementById('var').innerHTML = x.toString();
--- Webpage/javascript/index.js
+++ Webpage/javascript/index.js
@@ -1,3 +1,1 @@
 document.getElementById('date').innerHTML = new Date().toDateString();
-var x = 10;
-document.getElementById('var').innerHTML = x.toString();
